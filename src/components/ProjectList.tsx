0dd32f314c933748575cd8363f4daca2d7f84da8
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-07-28 22:37:27 +0700

    add color to project list

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,6 +1,32 @@
 import Image from "next/image";
 import styles from "./ProjectList.module.css";
+import { CSSProperties } from "react";
 
+const lineColors = ["green", "red", "blue"];
+
+function getUpDownStyle(index: number): CSSProperties {
+  const color = lineColors[index % lineColors.length];
+  return {
+    "--from": `var(--color-${color}-500)`,
+    "--to": `var(--color-${color}-950)`,
+  } as CSSProperties;
+}
+
+function getConnectStyle(index: number): CSSProperties {
+  const from = lineColors[index % lineColors.length];
+  const to = lineColors[(index + 1) % lineColors.length];
+  return {
+    "--from": `var(--color-${from}-950)`,
+    "--to": `var(--color-${to}-950)`,
+  } as CSSProperties;
+}
+
+function getImageStyle(index: number): CSSProperties {
+  const color = lineColors[index % lineColors.length];
+  return {
+    "--bg": `var(--color-${color}-500)`,
+  } as CSSProperties;
+}
 export interface ProjectItem {
   name: string;
   desc: string;
@@ -13,11 +39,20 @@ export interface ProjectItem {
 export default function ProjectList({ list }: { list: ProjectItem[] }) {
   return (
     <ul className={`clean-list ${styles.list}`}>
-      {list.map((item) => (
+      {list.map((item, index) => (
         <li key={item.name} className={styles.item}>
-          <div className={styles["up-line"]}></div>
-          <div className={styles["down-line"]}></div>
-          <div className={styles["connect-line"]}></div>
+          <div
+            className={styles["up-line"]}
+            style={getUpDownStyle(index)}
+          ></div>
+          <div
+            className={styles["down-line"]}
+            style={getUpDownStyle(index)}
+          ></div>
+          <div
+            className={styles["connect-line"]}
+            style={getConnectStyle(index)}
+          ></div>
           <div className={styles["item-detail"]}>
             <h3>{item.name}</h3>
             <p>{item.desc}</p>
@@ -51,7 +86,7 @@ export default function ProjectList({ list }: { list: ProjectItem[] }) {
               )}
             </div>
           </div>
-          <div className={styles["item-image"]}>
+          <div className={styles["item-image"]} style={getImageStyle(index)}>
             <Image width={250} height={250} src={item.image} alt={item.name} />
           </div>
         </li>
